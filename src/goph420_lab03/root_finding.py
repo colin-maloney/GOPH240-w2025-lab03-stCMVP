8e0bb4e984fdb11a1433fa9b45d8f2fd60f53553
Colin.Maloney <colin.maloney@ucalgary.ca>
2025-03-04 16:34:59 -0700

    root finder fixed maybe

--- src/goph420_lab03/root_finding.py
+++ src/goph420_lab03/root_finding.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 
-def root_newton_raphson(x0, f, dfdx): 
+def root_newton_raphson(x0, f, dfdx, tol=5e-6, max_iter=100): 
     """ 
     Parameters: 
     x0: float 
@@ -12,14 +12,22 @@ def root_newton_raphson(x0, f, dfdx):
         Function to evaluate the root 
     dfdx: numpy.ndarray
         Derivative of the function f
+    tol: float
+        Tolerance for the root 
+    max_iter: int
+        Maximum number of iterations
     Returns: 
     x: float 
         The root of the function   
     """
-    x = x0
-    tol = 5e-6 
-    max_iter = 100 
+    x = x0 
+
     for i in range(max_iter): 
-        x = x - f(x)/dfdx(x) 
-        if np.abs(f(x)) < tol: 
-            break
+       fx =f(x) 
+       dfx = dfdx(x) 
+
+       x_new = x - fx/dfx 
+
+       if abs(x_new - x) < tol: 
+            return x_new 
+    raise ValueError(f"Root not found within {max_iter} iterations")
