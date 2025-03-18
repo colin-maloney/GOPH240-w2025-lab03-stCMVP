a651b93d353d06e596287d828a11da914233ae9d
Viktor Pejic <viktor.pejic@ucalgary.ca>
2025-03-18 09:41:38 -0600

    modes done

--- examples/root_computation.py
+++ examples/root_computation.py
@@ -15,16 +15,16 @@ def main():
 
     freq = [0.1, 0.5, 1.0, 1.5, 2.0]
 
-    def F(Z,f):
+    def F(Z):
         C = (H ** 2) * (B1 ** -2 - B2 ** -2)
         return (((p2 / p1) * np.sqrt(C - Z ** 2) / Z)
                 - np.tan(2 * np.pi * f * Z))
-    def dfdz(Z,f):
+    def dfdz(Z):
         C = (H ** 2) * (B1 ** -2 - B2 ** -2)
         return ((-(p2/p1) * C/(Z**2 * np.sqrt(C - Z ** 2)))
                 - 2*np.pi*f*(1/np.cos(2*np.pi*f*Z))**2)
 
-    def asymptotes(f,H,B1,B2):
+    def get_asymptotes(f,H,B1,B2):
         atotes = [0.0]
         a = 0.0
         k = 0
@@ -39,20 +39,36 @@ def main():
         return atotes
 
     S_max = np.sqrt(H ** 2 * (B1 ** -2 - B2 ** -2))
-    initial_guess = []
-    for i in range(freq):
-        asymptotes = asymptotes(i,H,B1,B2)
-        F = F(Z, freq(i))
-        dfdz = dfdz(Z,freq(i))
-        for j in range(len(asymptotes)):
-            if asymptotes[j] != 0 or S_max:
-                x0 = asymptotes[j] - 1e-3
-                initial_guess.append(x0)
-                for j in range(len(initial_guess)):
-                    roots = []
-                    values = root_newton_raphson(initial_guess[j], F, dfdz)
-                    roots.append(values)
-                    print(roots)
+    root_modes = [[],[],[]]
+    for f in freq:
+        asymptotes = get_asymptotes(f,H,B1,B2)
+
+        def F(Z):
+            C = (H ** 2) * (B1 ** -2 - B2 ** -2)
+            return (((p2 / p1) * np.sqrt(C - Z ** 2) / Z)
+                    - np.tan(2 * np.pi * f * Z))
+
+        def dfdz(Z):
+            C = (H ** 2) * (B1 ** -2 - B2 ** -2)
+            return ((-(p2 / p1) * C / (Z ** 2 * np.sqrt(C - Z ** 2)))
+                    - 2 * np.pi * f * (1 / np.cos(2 * np.pi * f * Z)) ** 2)
+
+        initial_guess = []
+        for j, a in enumerate(asymptotes):
+            if a == 0 or (a == S_max and F(a) > 0):
+                continue
+            x0 = asymptotes[j] - 1e-3
+            initial_guess.append(x0)
+
+        roots = []
+        for x0 in initial_guess:
+            values = root_newton_raphson(x0, F, dfdz)[0]
+            roots.append(values)
+
+        for k, mode in enumerate(root_modes):
+            if k < len(roots):
+                mode.append(roots[k])
+    print(root_modes)
 
 
 
